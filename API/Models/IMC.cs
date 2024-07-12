93ac97faf123155eeef399eebcd02d62f127b11f
joao-moorgado <joao.moorgado@gmail.com>
2024-07-12 10:38:26 -0300

    Finalizacao back

--- API/Models/IMC.cs
+++ API/Models/IMC.cs
@@ -3,8 +3,73 @@ namespace API.Models;
 public class IMC
 {
     public string Id { get; set; }
-    public int Altura { get; set; }
+    public double Altura { get; set; }
     public int Peso { get; set; }
+    public double Valor => Peso / (Altura * Altura);
+    public string Classificacao
+    {
+        get
+        {
+            if (Valor < 18.5)
+            {
+                return "Magreza";
+            }
+            else if (Valor < 24.9)
+            {
+                return "Normal";
+            }
+            else if (Valor < 29.9)
+            {
+                return "Sobrepeso";
+            }
+            else if (Valor < 39.9)
+            {
+                return "Obesidade";
+            }
+            else
+            {
+                return "Obesidade Grave";
+            }
+        }
+    }
+    public string GrauObesidade
+    {
+        get
+        {
+            if (Valor < 18.5)
+            {
+                return "0";
+            }
+            else if (Valor < 24.9)
+            {
+                return "0";
+            }
+            else if (Valor < 29.9)
+            {
+                return "I";
+            }
+            else if (Valor < 39.9)
+            {
+                return "II";
+            }
+            else
+            {
+                return "III";
+            }
+        }
+    }
     public string AlunoId { get; set; }
     public Aluno Aluno { get; set; }
+
+    public IMC() { 
+        Id = Guid.NewGuid().ToString();
+    }
+
+    public IMC(double altura, int peso, string alunoId)
+    {
+        Id = Guid.NewGuid().ToString();
+        Altura = altura;
+        Peso = peso;
+        AlunoId = alunoId;
+    }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -26,9 +26,53 @@ app.MapPost("/api/aluno/cadastrar", ([FromBody] Aluno aluno, [FromServices] AppD
 
 app.MapPost("/api/imc/cadastrar", ([FromBody] IMC imc, [FromServices] AppDataContext ctx) =>
 {
+
+    imc.Aluno = ctx.Alunos.
+        Find(imc.AlunoId);
+    if (imc.Aluno == null)
+    {
+        return Results.BadRequest("Aluno não encontrado");
+    }
+
     ctx.IMCs.Add(imc);
     ctx.SaveChanges();
     return Results.Created($"/api/imc/obter/{imc.Id}", imc);
+});
+
+app.MapGet("/api/aluno/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.Alunos.Any()) 
+    {
+        return Results.Ok(ctx.Alunos.ToList());
+    }
+    return Results.NotFound("Nenhum aluno encontrado");
+});
+
+app.MapGet("/api/imc/listar", ([FromServices] AppDataContext ctx) =>
+{
+    if (ctx.IMCs.Any()) 
+    {
+        return Results.Ok(ctx.IMCs.ToList());
+    }
+    return Results.NotFound("Nenhum IMC encontrado");
+});
+
+app.MapGet("/api/imc/aluno/listar", ([FromServices] AppDataContext ctx) =>
+{
+
+});
+
+app.MapPatch("/api/imc/alterar/{id}", ([FromBody] IMC imc, [FromRoute] string id, [FromServices] AppDataContext ctx) =>
+{
+    var imcDB = ctx.IMCs.Find(id);
+    if (imcDB == null)
+    {
+        return Results.NotFound("IMC não encontrado");
+    }
+    imcDB.Altura = imc.Altura;
+    imcDB.Peso = imc.Peso;
+    ctx.SaveChanges();
+    return Results.Ok(imcDB);
 });
 
 //app.UseCors("Acesso Total");
